9ccd5235465485e4b9efea2680be37651c8c9b53
Papa Perry <pappaperrys@gmail.com>
2024-08-24 02:00:41 -0500

    df

--- theappdepo/settings.py
+++ theappdepo/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'profiles',
-    'django.contrib.admin',
+ #   'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
