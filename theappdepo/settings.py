fa9590e63c0bf839a58a4410ebda73bd1106437c
Papa Perry <pappaperrys@gmail.com>
2024-08-24 01:50:12 -0500

    asdf

--- theappdepo/settings.py
+++ theappdepo/settings.py
@@ -166,7 +166,7 @@ AUTH_USER_MODEL = 'profiles.User_Profile'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT=os.path.join(BASE_DIR,"/static/") 
+STATIC_ROOT=os.path.join(BASE_DIR,"/collectstatic/") 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
